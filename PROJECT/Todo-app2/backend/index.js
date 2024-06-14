a596450e40d4a8a28a1ccdbde8b67ddc6fc1ae81
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-06-14 18:32:20 +0530

    Todo rest of frontent added

--- PROJECT/Todo-app2/backend/index.js
+++ PROJECT/Todo-app2/backend/index.js
@@ -1,65 +1,63 @@
-const express=require("express");
+const express = require("express");
 const { createTodo, updateTodo } = require("./types");
-const {todo} =require("./db")
-const app=express();
-const cors=require("cors")
+const { todo } = require("./db")
+const app = express();
+const cors = require("cors")
 app.use(express.json());
-app.use(cors({
-    origin:"http://localhost:5173/"
-}));
+app.use(cors());
 
-app.post('/todo',async function(req,res){
- const createPayload=req.body;
- const parsedPayload=createTodo.safeParse(createPayload);
- if(!parsedPayload.success){
-    res.status(411).json({
-     msg:"You sent wrong inputs"
+app.post('/todo', async function (req, res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent wrong inputs"
+        })
+        return;
+    }
+    //put in db
+
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
     })
-  return;  
- }
- //put in db
-
- await todo.create({
-    title:createPayload.title,
-    description:createPayload.description,
-    completed:false
- })
- res.json({
-    msg:"Todo created!"
- })
+    res.json({
+        msg: "Todo created!"
+    })
 })
 
-app.get('/todo',async function(req,res){
-  
+app.get('/todo', async function (req, res) {
 
-  const todos=await todo.find({});
-  res.json({
-    todos
-  })
+
+    const todos = await todo.find({});
+    res.json({
+        todos
+    })
 })
 
-app.put('/completed',async function(req,res){
-    const createPayload=req.body;
-    const parsedPayload=updateTodo.safeParse(createPayload);
-    if(!parsedPayload.success){
-       res.status(411).json({
-        msg:"You sent wrong inputs"
-       })
-     return;  
+app.put('/completed', async function (req, res) {
+    const createPayload = req.body;
+    const parsedPayload = updateTodo.safeParse(createPayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent wrong inputs"
+        })
+        return;
     }
 
     //update the end point and set it to be true
 
-await todo.update({
-    _id:req.body.id
-},{
-    completed:true
-})
-res.json({
-    msg:"Todo marked as completed!"
-})
+    await todo.update({
+        _id: req.body.id
+    }, {
+        completed: true
+    })
+    res.json({
+        msg: "Todo marked as completed!"
+    })
 
-    
+
 })
 
 app.listen(3000);

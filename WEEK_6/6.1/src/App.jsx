43aee0061b32c0cd382dd51eb24003e58d2ab4c8
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-06-18 15:28:00 +0530

    Solution to minimize the no. of re-renders in the app

--- WEEK_6/6.1/src/App.jsx
+++ WEEK_6/6.1/src/App.jsx
@@ -2,25 +2,37 @@ import { useState } from 'react'
 
 
 function App() {
-const [title,setTitle]=useState("My name is Aishwarya");
 
-function updateTitle(){
- setTitle("My name is " + Math.random());
-}
+
 
   return (
-    
-      <div>
-        <button onClick={updateTitle}>Click me to change title</button>
-        <Header title={title}></Header>
-        <Header title="My name is Aditya"></Header>
-      </div>
-    
+
+    <div>
+      <HeaderWithButton></HeaderWithButton>
+      <Header title="My name is Kirat"></Header>
+      <Header title="My name is Kirat"></Header>
+      <Header title="My name is Kirat"></Header>
+      <Header title="My name is Aditya"></Header>
+
+    </div>
+
   )
 }
 
+function HeaderWithButton() {
+  const [firstTitle, setFirstTitle] = useState("My name is Aishwarya");
+  function changeTitle() {
+    setFirstTitle("My name is " + Math.random());
+  }
+  return (
+    <div>
+      <button onClick={changeTitle}>Click me to change title</button>
+      <Header title={firstTitle}></Header>
+    </div>
+  )
+}
 
-function Header(props){
+function Header(props) {
   return <div>
     {props.title}
   </div>

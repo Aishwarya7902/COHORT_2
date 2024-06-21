109e86a32464a82464164cef033c8b00c904a620
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-06-21 12:34:28 +0530

    ugly syntax for wrapper components

--- WEEK_6/6.1/src/App.jsx
+++ WEEK_6/6.1/src/App.jsx
@@ -3,49 +3,28 @@ import React from 'react';
 
 
 function App() {
-
- const [todos,setTodods]=useState([
-  {
-    id:1,
-    tile:"go to gym ",
-    description:"go to gym from 7-9"
-  },
-  {
-    id:2,
-    tile:"go to park ",
-    description:"go to park from 9-10"
-  },
-  {
-    id:3,
-    tile:"study",
-    description:"study from 10-2"
-  }
- ])
-
-function addTodo(){
-  setTodods([...todos,{
-    id:4,
-    title:Math.random(),
-    description:Math.random()
-  }])
-}
-  return (
-
-    <div>
-       <button onClick={addTodo}>Add a todo</button>
-      {todos.map(function (todo){
-        return <Todo title={todo.title} description={todo.description}/>
-      })}
-    </div>
-
-  )
+return <div>
+  <CardWrapper innerComponent={<TextComponent/>}></CardWrapper>
+  <CardWrapper innerComponent={<TextComponent2/>}></CardWrapper>
+</div> 
 }
 
-function Todo({title,description}){
- return <div>
-   <h1>{title}</h1>
-   <h5>{description}</h5>
- </div>
+function TextComponent(){
+  return <div>
+     Hi there!
+  </div>
+}
+
+function TextComponent2(){
+  return <div>
+     Hi there2!
+  </div>
+}
+
+function CardWrapper({innerComponent}){
+  return <div style={{border:"2px black solid", padding:20}}>
+    {innerComponent}
+  </div>
 }
 
 export default App

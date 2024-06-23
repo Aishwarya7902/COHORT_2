5b6bd72b83783efe385bb08da69040c4b24d401a
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-06-23 15:15:20 +0530

    solution for assignment-Create an app that polls the sum server Gets the current set of TODOs Renders it on screen

--- WEEK_6/6.1/src/App.jsx
+++ WEEK_6/6.1/src/App.jsx
@@ -5,25 +5,30 @@ import React from 'react';
 function App() {
 const [todos,setTodos]=useState([]);
 
-useEffect(()=>{
-  fetch("https://sum-server.100xdevs.com/todos")
-  .then(async function(res){
-    const json=await res.json();
-    setTodos(json.todos);
-  })
-})
+  useEffect(()=>{
+    setInterval(()=>{
+      fetch("https://sum-server.100xdevs.com/todos")
+      .then(async function(res){
+        const json=await res.json();
+        setTodos(json.todos);
+      })
+    },10000)
+  },[])
+  
+
+
 
 return <div>
  
-  {todos.map(todo => <Todo title={todos.title} description={todos.description}></Todo>)}
+  {todos.map(todo => <Todo key={todo.id}  title={todo.title} description={todo.description}></Todo>)}
 </div> 
 }
 
 
 function Todo({title,description}){
   return <div >
-    <h1>title</h1>
-    <h4>description</h4>
+    <h1>{title}</h1>
+    <h4>{description}</h4>
   </div>
 }
 

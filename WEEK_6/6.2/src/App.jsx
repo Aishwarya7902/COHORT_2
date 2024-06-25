bb73e2d8e6a9ef3f9a8891850c68a944455ab5ab
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-06-26 00:10:48 +0530

    assignment 1(page 15 of slides)

--- WEEK_6/6.2/src/App.jsx
+++ WEEK_6/6.2/src/App.jsx
@@ -4,27 +4,31 @@ import { useEffect } from 'react';
 import axios from "axios"
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-   useEffect(()=>{
-    axios.get("https://sum-server.100xdevs.com/todos")
-    .then(function(response){
-      setTodos(response.data.todos)
-    })
-   },[])
-  
-  return (
-    <>
-      {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description}></Todo>)}
-    </>
-  )
-}
-
-function Todo({ title, description }) {
   return <div>
-    <h1>{title}</h1>
-    <h4>{description}</h4>
+    <Todo id={1}></Todo>
+
   </div>
 }
 
-export default App
+function Todo({id}) {
+  const [todos, setTodos] = useState({});
+
+  useEffect(() => {
+    axios.get("https://sum-server.100xdevs.com/todo?id=" + id)
+   .then(response=>{
+     setTodos(response.data.todo)
+   })
+
+  }, [])
+
+  return <div>
+    <h1>
+      {todos.title}
+    </h1>
+    <h4>
+      {todos.description}
+    </h4>
+  </div>
+}
+
+export default App;

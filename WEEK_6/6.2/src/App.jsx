dcf12e3eb95e446a1d3d2c4a0b7a73f8f7cfc494
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-06-25 20:02:24 +0530

    BackEnd application with axios in react app

---
+++ WEEK_6/6.2/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+import { useEffect } from 'react';
+import axios from "axios"
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+   useEffect(()=>{
+    axios.get("https://sum-server.100xdevs.com/todos")
+    .then(function(response){
+      setTodos(response.data.todos)
+    })
+   },[])
+  
+  return (
+    <>
+      {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description}></Todo>)}
+    </>
+  )
+}
+
+function Todo({ title, description }) {
+  return <div>
+    <h1>{title}</h1>
+    <h4>{description}</h4>
+  </div>
+}
+
+export default App

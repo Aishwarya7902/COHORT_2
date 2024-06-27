3ac156f41c36321beaed05781000d4c9f7c04397
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-06-27 18:53:09 +0530

    Ugly solution for assignment (slide p.no 22)

--- WEEK_6/6.2/src/App.jsx
+++ WEEK_6/6.2/src/App.jsx
@@ -4,43 +4,26 @@ import { useEffect } from 'react';
 import axios from "axios"
 
 function App() {
-  const [id,setId]=useState(1)
+  const [counter,setCounter]=useState(0)
+  const [inputValue,setInputValue]=useState(1);
+  let count=0;
+  for(let i=1;i<=inputValue;i++){
+    count+=i;
+  }
   return <div>
 
-    {/* <button onClick={<Todo id={1}></Todo>}>1</button>
-    <button onClick={<Todo id={2}></Todo>}>2</button>
-    <button onClick={<Todo id={3}></Todo>}>3</button>
-    <button onClick={<Todo id={4}></Todo>}>4</button> */}
-    <button onClick={()=>setId(1)}>1</button>
-    <button onClick={()=>setId(2)}>2</button>
-    <button onClick={()=>setId(3)}>3</button>
-    <button onClick={()=>setId(4)}>4</button>
-    <Todo id={id}></Todo>
-
+    <input onChange={function(e){
+      setInputValue(e.target.value)
+    }} type="text" placeholder='Find sum from 1 to n'/>
+    
+    <br />
+    <h3>{"Sum from 1 to " + inputValue + " is  " +  count}</h3>
+    <button onClick={()=>setCounter(counter+1)}>Counter {counter}</button>
     
 
   </div>
 }
 
-function Todo({id}) {
-  const [todos, setTodos] = useState({});
 
-  useEffect(() => {
-    axios.get("https://sum-server.100xdevs.com/todo?id=" + id)
-   .then(response=>{
-     setTodos(response.data.todo)
-   })
-
-  },[id])
-
-  return <div>
-    <h1>
-      {todos.title}
-    </h1>
-    <h4>
-      {todos.description}
-    </h4>
-  </div>
-}
 
 export default App;

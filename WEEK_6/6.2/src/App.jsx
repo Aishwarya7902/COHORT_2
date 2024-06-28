4b43d7fb0b73e6fcc04ddd670503a16313abb0eb
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-06-28 13:35:52 +0530

    use memo example

--- WEEK_6/6.2/src/App.jsx
+++ WEEK_6/6.2/src/App.jsx
@@ -2,24 +2,30 @@ import { useState } from 'react'
 import './App.css'
 import { useEffect } from 'react';
 import axios from "axios"
+import { useMemo } from 'react';
 
 function App() {
-  const [counter,setCounter]=useState(0)
-  const [inputValue,setInputValue]=useState(1);
-  let count=0;
-  for(let i=1;i<=inputValue;i++){
-    count+=i;
-  }
+  const [counter, setCounter] = useState(0)
+  const [inputValue, setInputValue] = useState(1);
+
+  let count = useMemo(() => {
+    let finalCount = 0;
+    for (let i = 1; i <= inputValue; i++) {
+       finalCount+= i;
+    }
+    return finalCount;
+  }, [inputValue]);
+
   return <div>
 
-    <input onChange={function(e){
+    <input onChange={function (e) {
       setInputValue(e.target.value)
-    }} type="text" placeholder='Find sum from 1 to n'/>
-    
+    }} type="text" placeholder='Find sum from 1 to n' />
+
     <br />
-    <h3>{"Sum from 1 to " + inputValue + " is  " +  count}</h3>
-    <button onClick={()=>setCounter(counter+1)}>Counter {counter}</button>
-    
+    <h3>{"Sum from 1 to " + inputValue + " is  " + count}</h3>
+    <button onClick={() => setCounter(counter + 1)}>Counter {counter}</button>
+
 
   </div>
 }

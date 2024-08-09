b6e213d7801fc9b2c420e5bc14a192de0dc04391
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-08-09 14:00:50 +0530

    responsiveness in tailwind

--- WEEK_8/tailwind_practice/src/App.jsx
+++ WEEK_8/tailwind_practice/src/App.jsx
@@ -2,19 +2,17 @@
 import './App.css'
 
 function App() {
- 
+
 
   return (
     <div>
-          
+      <div className='grid grid-cols-1 md:grid-cols-3'>
+      
+        <div className='bg-red-500 '>Hi from div 1</div>
+        <div className='bg-blue-500 '>Hi from div 1</div>
+        <div className='bg-green-500 '>Hi from div 1</div>
+      </div>
 
-          <div className='grid grid-cols-12'>
-            <div className='bg-red-500 col-span-5'>Hi from div 1</div>
-            <div className='bg-blue-500 col-span-5'>Hi from div 2</div>
-            <div className='bg-green-500 col-span-2'>Hi from div 3</div>
-            
-            
-          </div>
     </div>
   )
 }

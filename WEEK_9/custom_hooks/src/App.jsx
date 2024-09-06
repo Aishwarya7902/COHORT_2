d498f05bad6574bbad09f26de9c553a703f00529
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-09-06 15:40:24 +0530

    making a data fetching custom hook

--- WEEK_9/custom_hooks/src/App.jsx
+++ WEEK_9/custom_hooks/src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function useTodos() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    axios.get("https://sum-server.100xdevs.com/todos")
+      .then(res => {
+        setTodos(res.data.todos);
+      })
+  }, [])
+
+  return todos;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const todos = useTodos();
 
   return (
     <>
-      
+      {todos.map(todo => <Track todo={todo} />)}
     </>
   )
+}
+
+function Track({ todo }) {
+  return <div>
+    {todo.title}
+    <br />
+    {todo.description}
+  </div>
 }
 
 export default App

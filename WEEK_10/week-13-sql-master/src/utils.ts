c5487787f0fc211393848d28eef1617f6e68349b
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-09-16 20:26:06 +0530

    Made some changes

--- WEEK_10/week-13-sql-master/src/utils.ts
+++ WEEK_10/week-13-sql-master/src/utils.ts
@@ -1,7 +1,9 @@
 import { Client } from 'pg';
+require('dotenv').config();
+const dbUrl = process.env.DATABASE_URL;
 
 export async function getClient() {
-    const client = new Client("postgresql://test_owner:Q8tNjnHB4sCZ@ep-flat-forest-a5i7lffx.us-east-2.aws.neon.tech/test?sslmode=require");
+    const client = new Client(dbUrl);
     await client.connect();
     return client;
 }

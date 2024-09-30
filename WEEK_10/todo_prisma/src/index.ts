aeedba017ccdbce915b133121704c3ad2c9ac9c2
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-09-30 20:21:18 +0530

    updation on user

--- WEEK_10/todo_prisma/src/index.ts
+++ WEEK_10/todo_prisma/src/index.ts
@@ -1,16 +1,27 @@
 import { PrismaClient } from "@prisma/client";
-const prisma=new PrismaClient();
 
-async function insertUser(username: string, password: string, firstName: string, lastName: string) {
-    const res = await prisma.user.create({
-      data: {
-          email:username,
-          password,
-          firstName,
-          lastName
-      }
-    })
-    console.log(res);
-  }
-  
-  insertUser("admin1", "123456", "harkirat", "singh")
+const prisma = new PrismaClient();
+
+interface UpdateParams {
+    firstName: string;
+    lastName: string;
+}
+
+async function updateUser(username: string, {
+    firstName,
+    lastName
+}: UpdateParams) {
+  const res = await prisma.user.update({
+    where: { email:username },
+    data: {
+      firstName,
+      lastName
+    }
+  });
+  console.log(res);
+}
+
+updateUser("admin1", {
+    firstName: "new name",
+    lastName: "singh"
+})

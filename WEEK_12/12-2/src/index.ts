c8968eb08ae1fa030bad7198e997bbfd08d7028b
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-10-09 18:54:08 +0530

    final

--- WEEK_12/12-2/src/index.ts
+++ WEEK_12/12-2/src/index.ts
@@ -6,7 +6,8 @@ interface User{
     password:string;
 };
 
-type updateProps=Pick<User,'name' | 'email' | 'age'>
+type updateProps=Pick<User,'name' | 'email' | 'age'> //it picks properties from an existing type
+type updateOptionalPros=Partial<updateProps> //partial makes all the properties as optional
 
 function updateUser(updateProps:updateProps){
     //db call to update user 
